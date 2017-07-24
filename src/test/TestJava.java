1847be2145e0f22283eae889636a1bfcbdb110bd
Jikaworld <dacasnake@gmail.com>
2017-07-24 11:20:39 -0500

    test 1
    add more println

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -3,7 +3,10 @@ package test;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.print("hello world");
+		System.out.println("hello world");
+		System.out.println("hello jenkins");
+		System.out.println("hello asdsad");
+		System.out.println("hello world");
 		// TODO Auto-generated method stub
 
 	}
